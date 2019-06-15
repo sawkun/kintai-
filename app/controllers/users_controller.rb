65e9d937bc36fcdbd6bd8c4ca1555e5f416ac1d0
sawkun <saru931aaa@yahoo.co.jp>
2019-06-15 07:40:36 +0000

    8.2finn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only[:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
   before_action :logged_in_user, only: [:show, :edit, :update]
-  before_action :correct_user, only[:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
   end
@@ -52,6 +52,6 @@ class UsersController < ApplicationController
    
    def correct_user
      @user = User.find(params[:id])
-     redirect_to root_url unless current_user?(@user)
-     end
+     redirect_to(root_url) unless current_user?(@user)
+   end
 end
